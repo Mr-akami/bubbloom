48a2cceb3ceca9335db9f9d57dfaa5091d006aef
hiro0118 <fuji.hiroshi0118@gmail.com>
2021-05-16 21:25:14 +0900

    Gradle & Spring Boot テンプレート追加

---
+++ src/main/kotlin/com/example/ItinerantConvivial/ItinerantConvivialApplication.kt
@@ -0,0 +1,11 @@
+package com.example.ItinerantConvivial
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ItinerantConvivialApplication
+
+fun main(args: Array<String>) {
+	runApplication<ItinerantConvivialApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/ItinerantConvivial/ItinerantConvivialApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.ItinerantConvivial
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ItinerantConvivialApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
