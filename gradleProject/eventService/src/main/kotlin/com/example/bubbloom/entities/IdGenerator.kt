ff881c15ded24db6d4495f26d4f88bebb35b04aa
hiro0118 <fuji.hiroshi0118@gmail.com>
2021-09-19 23:39:59 +0900

    #11 Change id generatoin logic to UUID

--- gradleProject/eventService/src/main/kotlin/com/example/bubbloom/entities/IdGenerator.kt
+++ gradleProject/eventService/src/main/kotlin/com/example/bubbloom/entities/IdGenerator.kt
@@ -6,13 +6,7 @@ import java.util.*
 @Component
 class IdGenerator {
 
-    companion object {
-        private const val MAX_ID = 99999999;
-    }
-
-    private val rand = Random()
-
     fun generate(): String {
-        return String.format("%08d", rand.nextInt(MAX_ID + 1))
+        return UUID.randomUUID().toString()
     }
 }
--- gradleProject/eventService/src/main/resources/db/create_event_db.sql
+++ gradleProject/eventService/src/main/resources/db/create_event_db.sql
@@ -3,6 +3,7 @@ CREATE DATABASE IF NOT EXISTS event_service_db;
 USE event_service_db;
 
 CREATE TABLE IF NOT EXISTS event (
-    id VARCHAR(8) NULL,
-    title VARCHAR(30) NOT NULL
+    id VARCHAR(36) NOT NULL,
+    title VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
 );
