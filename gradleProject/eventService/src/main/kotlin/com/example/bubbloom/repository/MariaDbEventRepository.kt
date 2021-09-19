46e7f9fb89d993422659b225a1e27587417318d4
hiro0118 <fuji.hiroshi0118@gmail.com>
2021-09-19 23:02:07 +0900

    #11 Add JOOQ and simplify repository class

--- gradleProject/eventService/src/main/kotlin/com/example/bubbloom/repository/MariaDbEventRepository.kt
+++ gradleProject/eventService/src/main/kotlin/com/example/bubbloom/repository/MariaDbEventRepository.kt
@@ -2,10 +2,12 @@ package com.example.bubbloom.repository
 
 import com.example.bubbloom.entities.Event
 import com.example.bubbloom.service.IEventRepository
+import org.jooq.impl.DSL
+import org.jooq.impl.DSL.field
+import org.jooq.impl.DSL.table
 import org.springframework.stereotype.Repository
+import java.sql.Connection
 import java.sql.DriverManager
-import java.sql.ResultSet
-import kotlin.collections.ArrayList
 
 @Repository
 class MariaDbEventRepository() : IEventRepository {
@@ -16,72 +18,63 @@ class MariaDbEventRepository() : IEventRepository {
         private const val USER = "root"
         private const val PASS = "root"
         private const val DATABASE = "event_service_db"
-        private const val TABLE = "event"
         private const val URL = "jdbc:mariadb://$ADDRESS:$PORT/$DATABASE"
-        private const val INSERT_QUERY = "INSERT INTO $TABLE (id, title) VALUES (?, ?);"
-        private const val GET_QUERY = "SELECT * FROM $TABLE WHERE id=?;"
-        private const val GET_ALL_QUERY = "SELECT * FROM $TABLE;"
-        private const val UPDATE_QUERY = "UPDATE $TABLE SET title = ? WHERE id = ?;"
-        private const val DELETE_QUERY = "DELETE FROM $TABLE WHERE id=?;"
+
+        private val EVENT_TABLE = table("event")
+        private val ID_FIELD = field("id")
+        private val TITLE_FIELD = field("title")
+    }
+
+    private fun connectToDB(): Connection {
+        return DriverManager.getConnection(URL, USER, PASS)
     }
 
     override fun save(event: Event) {
-        DriverManager.getConnection(URL, USER, PASS).use { conn ->
-            conn.prepareStatement(INSERT_QUERY).use { statement ->
-                statement.setString(1, event.id)
-                statement.setString(2, event.title)
-                statement.executeUpdate()
-            }
+        connectToDB().use { conn ->
+            DSL.using(conn)
+                .insertInto(EVENT_TABLE, ID_FIELD, TITLE_FIELD)
+                .values(event.id, event.title)
+                .execute()
         }
     }
 
     override fun get(id: String): Event? {
-        DriverManager.getConnection(URL, USER, PASS).use { conn ->
-            conn.prepareStatement(GET_QUERY).use { statement ->
-                statement.setString(1, id)
-                statement.executeQuery().use { resultSet ->
-                    return if (resultSet.next()) buildEventFrom(resultSet) else null
-                }
-            }
+        connectToDB().use { conn ->
+            return DSL.using(conn)
+                .select()
+                .from(EVENT_TABLE)
+                .where(field("id").eq(id))
+                .fetchOne()
+                ?.into(Event::class.java)
         }
     }
 
     override fun getAll(): List<Event> {
-        val events = ArrayList<Event>()
-        DriverManager.getConnection(URL, USER, PASS).use { conn ->
-            conn.createStatement().use { statement ->
-                statement.executeQuery(GET_ALL_QUERY).use { resultSet ->
-                    while (resultSet.next()) {
-                        events.add(buildEventFrom(resultSet))
-                    }
-                }
-            }
+        connectToDB().use { conn ->
+            return DSL.using(conn)
+                .select()
+                .from(EVENT_TABLE)
+                .fetch()
+                .into(Event::class.java)
         }
-        return events
     }
 
     override fun update(id: String, event: Event) {
-        DriverManager.getConnection(URL, USER, PASS).use { conn ->
-            conn.prepareStatement(UPDATE_QUERY).use { statement ->
-                statement.setString(1, event.title)
-                statement.setString(2, id)
-                statement.executeUpdate()
-            }
+        connectToDB().use { conn ->
+            DSL.using(conn)
+                .update(EVENT_TABLE)
+                .set(TITLE_FIELD, event.title)
+                .where(ID_FIELD.eq(event.id))
+                .execute()
         }
     }
 
     override fun delete(id: String) {
-        DriverManager.getConnection(URL, USER, PASS).use { conn ->
-            conn.prepareStatement(DELETE_QUERY).use { statement ->
-                statement.setString(1, id)
-                statement.executeUpdate()
-            }
+        connectToDB().use { conn ->
+            DSL.using(conn)
+                .delete(EVENT_TABLE)
+                .where(ID_FIELD.eq(id))
+                .execute()
         }
-    }
-
-    private fun buildEventFrom(resultSet: ResultSet): Event {
-        val id = resultSet.getString("id")
-        val title = resultSet.getString("title")
-        return Event(id, title)
     }
 }
